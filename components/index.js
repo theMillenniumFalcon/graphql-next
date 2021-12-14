d08cac8a7e5e077f071862d765d01df690fdc29a
Nishank <nishankpr2002@gmail.com>
2021-12-14 11:08:44 +0530

    styled_added

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as PostCard } from './postcard/PostCard'
+export { default as Categories } from './categories/Categories'
+export { default as PostWidget } from './postwidget/PostWidget'
