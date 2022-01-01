3e434490ae5023edfb7b4e8deee64b434e8d2933
Nishank <nishankpr2002@gmail.com>
2022-01-02 01:31:22 +0530

    minor changes

--- components/commentsform/CommentsForm.jsx
+++ components/commentsform/CommentsForm.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+    const [error, setError] = useState(false)
+    const [localStorage, setLocalStorage] = useState(null)
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const commentEl = useRef()
+    const nameEl = useRef()
+    const emailEl = useRef()
+    const storeDataEl = useRef()
+
+    const handlePostSubmission = () => {
+        setError(false);
+        const { name, email, comment, storeData } = formData;
+        if (!name || !email || !comment) {
+          setError(true);
+          return;
+        }
+        const commentObj = { name, email, comment, slug }
+    
+        if (storeData) {
+          localStorage.setItem('name', name)
+          localStorage.setItem('email', email)
+        } else {
+          localStorage.removeItem('name')
+          localStorage.removeItem('email')
+        }
+    
+        submitComment(commentObj)
+          .then((res) => {
+            if (res.createComment) {
+              if (!storeData) {
+                formData.name = ''
+                formData.email = ''
+              }
+              formData.comment = ''
+              setFormData((prevState) => ({
+                ...prevState,
+                ...formData,
+              }))
+              setShowSuccessMessage(true);
+              setTimeout(() => {
+                setShowSuccessMessage(false);
+              }, 3000)
+            }
+          })
+      }
+
     return (
-        <div>
-            CommentsForm
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+            <div className="grid-cols-1 gap-4 mb-4">
+                <textarea 
+                    ref={commentEl} 
+                    className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                    placeholder="Comment"
+                    name="comment"
+                />
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+                <input 
+                    type="text" ref={nameEl}
+                    className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                    placeholder="Name"
+                    name="name"
+                />
+                <input 
+                    type="text" ref={emailEl}
+                    className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                    placeholder="Email"
+                    name="email"
+                />
+            </div>
+            {error && <p className="text-xs text-red-500">All fields are required</p>}
+            <div className="mt-8">
+                <button 
+                    type="button" 
+                    onClick={handleCommmentSubmit}
+                    className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+                >
+                    Post Comment
+                </button>
+                {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
+            </div>
         </div>
     )
 }
